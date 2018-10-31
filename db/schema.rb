e57a42d5a4b6ea7b20a597aee1b8c7138c0c8757
MC Le Morvan <mclemorvan@gmail.com>
2018-10-31 21:10:32 +0100

    header fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_31_174008) do
+ActiveRecord::Schema.define(version: 2018_10_31_195948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2018_10_31_174008) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.bigint "gossip_id"
+    t.string "anonymous_commentor"
     t.index ["gossip_id"], name: "index_comments_on_gossip_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
